570c8bb13b05f997ee99a18f225143a63bb6af1b
claire lenglart <djanetdesign@gmail.com>
2024-03-15 10:50:38 +0100

    front index & show

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,6 +4,8 @@ class Space < ApplicationRecord
   has_many :space_ratings, dependent: :destroy
   has_many_attached :photos
 
+  validates :photos, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
