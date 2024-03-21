8a4f4f927f75de71583a257aa0639cac4488f8f5
claire lenglart <djanetdesign@gmail.com>
2024-03-21 18:15:19 +0100

    resa finished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
-  has_many :booking_dates
+  has_many :booking_dates, dependent: :destroy
   # accepts_nested_attributes_for :booking_dates
 end
